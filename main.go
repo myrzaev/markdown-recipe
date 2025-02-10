7b6efcc25c89b875aa6fb4fca07a60af45f972f0
Timur Myrzaev <tmyrzaev99@gmail.com>
2025-02-09 23:57:02 -0500

    embeded index html file

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	_ "embed"
 	"log"
-	"log/slog"
 	"net/http"
-	"os"
 )
+
+//go:embed index.html
+var html string
 
 func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("/", defaultOne)
+
 	err := http.ListenAndServe(":8000", router)
 	if err != nil {
 		log.Fatal(err)
@@ -17,11 +20,5 @@ func main() {
 }
 
 func defaultOne(w http.ResponseWriter, r *http.Request) {
-	f, err := os.ReadFile("index.html")
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		slog.Error("failed to get index.html", "filename", "index.html")
-		return
-	}
-	w.Write(f)
+	w.Write([]byte(html))
 }
